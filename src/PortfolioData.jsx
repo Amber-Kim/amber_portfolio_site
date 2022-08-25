e734a9819d513b7268b1b6d682eaccdfda0a6bec
Amber-Kim <amber.kje@gmail.com>
2022-08-25 10:54:41 -0400

    changed

--- src/PortfolioData.jsx
+++ src/PortfolioData.jsx
@@ -1,43 +1,28 @@
 import proj1 from "images/fyp.png";
 import proj2 from "images/netflix.PNG";
-import proj3 from "images/stripe.PNG";
-import proj4 from "images/covid.png";
+import proj3 from "images/reddit.png";
+import proj4 from "images/portfolio.png";
 import proj5 from "images/googleMap.png";
 import proj6 from "images/vet.png";
 
 
 const PortfolioData = [
-  {
-    id: 1,
-    title: "FYP EasyGet App",
-    tagline: "EasyGet App with Prime Videos",
-    liveUrl: "https://github.com/Amber-Kim",
-    repositoryUrl: "https://github.com/Amber-Kim",
-    image: proj1,
-  },
-  {
-    id: 2,
-    title: "Netflix Clone",
-    tagline: "Netflix Clone with React JS",
-    liveUrl: "https://github.com/Amber-Kim",
-    repositoryUrl: "https://github.com/Amber-Kim",
-    image: proj2,
-  },
+
   {
     id: 3,
-    title: "Stripe Menu",
-    tagline: "STRIPE SUBMENUS UI",
-    liveUrl: "https://github.com/Amber-Kim",
-    repositoryUrl: "https://github.com/Amber-Kim",
+    title: "Reddit Clone Deploy project",
+    tagline: "Full-stack website using NextJS, Typescript, Express JS, and Postgresql",
+    liveUrl: "https://github.com/Amber-Kim/reddit-clone-deploy",
+    repositoryUrl: "http://ec2-18-116-49-47.us-east-2.compute.amazonaws.com/",
     image: proj3,
   },
 
   {
     id: 4,
-    title: "COVID-19 Tracker",
-    tagline: "Information about Active Cases, Deaths, Recovered due to Covid-19 globally",
-    liveUrl: "https://github.com/Amber-Kim",
-    repositoryUrl: "https://github.com/Amber-Kim",
+    title: "Amber's Portfolio Site",
+    tagline: "Information about my experience, education, and technical skills",
+    liveUrl: "https://github.com/Amber-Kim/amber_portfolio_site",
+    repositoryUrl: "https://jeongeun-kim.netlify.app/",
     image: proj4,
   },
   
