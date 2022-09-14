b98d7f452db17eb1017bbad3c5a8f338c693d9c8
Amber-Kim <[amber.kje@gmail.com]>
2022-09-14 19:39:00 -0400

    Updated

--- src/PortfolioData.jsx
+++ src/PortfolioData.jsx
@@ -1,10 +1,11 @@
 import proj1 from "images/reddit.png";
-import proj2 from "images/telegram.png";
-import proj3 from "images/portfolio.png";
-import proj4 from "images/blog.png";
-import proj5 from "images/crm.png";
-import proj6 from "images/googleMap.png";
-import proj7 from "images/vet.png";
+import proj2 from "images/google_travel.png";
+import proj3 from "images/telegram.png";
+import proj4 from "images/portfolio.png";
+import proj5 from "images/blog.png";
+import proj6 from "images/crm.png";
+import proj7 from "images/googleMap.png";
+import proj8 from "images/vet.png";
 
 
 const PortfolioData = [
@@ -13,7 +14,7 @@ const PortfolioData = [
   {
     id: 1,
     title: "Reddit Clone Deploy Project",
-    tagline: "Full stack website using NextJS, Typescript, Express JS, and PostgrSQL",
+    tagline: "Full Stack Web Application using NextJS, Typescript, NodeJS & Express, and PostgrSQL",
     liveUrl: "http://ec2-3-15-32-248.us-east-2.compute.amazonaws.com/",
     repositoryUrl: "https://github.com/Amber-Kim/reddit-clone-deploy",
     image: proj1,
@@ -21,55 +22,63 @@ const PortfolioData = [
 
   {
     id: 2,
+    title: "Google Map Travel Recommander",
+    tagline: "Web Application with React Hooks, and Refs working with Google Maps API & Google Places API, and fetching data from RapidAPI",
+    liveUrl: "https://google-travel-recommander.netlify.app/",
+    repositoryUrl: "https://github.com/Amber-Kim/Google-map-restaurant-API",
+    image: proj2,
+  },
+  {
+    id: 3,
     title: "Telegram Bot Automatic Crawling",
     tagline: "Created Web and sent message automatically through Telegram_bot by using Django Rest Framework API with beautifulsoup ps4 of Python",
     liveUrl: "http://99.79.75.233:8000/",
     repositoryUrl: "https://github.com/Amber-Kim/hotdeal_bot",
-    image: proj2,
+    image: proj3,
   },
 
   {
-    id: 3,
+    id: 4,
     title: "Amber's Portfolio Site",
     tagline: "Created by ReactJSX, React Framer Motion, aos library, and emailjs.",
     liveUrl: "https://jeongeun-kim.netlify.app/",
     repositoryUrl: "https://github.com/Amber-Kim/amber_portfolio_site",
-    image: proj3,
-  },
-  
-  {
-    id: 4,
-    title: "NextJS - Small Blog Practice",
-    tagline: "Tutorial with React, Nextjs, TypeScript, Markdown, CSS Module",
-    liveUrl: "https://luminous-baklava-0b0119.netlify.app/",
-    repositoryUrl: "https://github.com/Amber-Kim/nextjs_blog",
     image: proj4,
   },
   
   {
     id: 5,
+    title: "NextJS - Small Blog Practice",
+    tagline: "Tutorial with React, Nextjs, TypeScript, Markdown, CSS Module",
+    liveUrl: "https://luminous-baklava-0b0119.netlify.app/",
+    repositoryUrl: "https://github.com/Amber-Kim/nextjs_blog",
+    image: proj5,
+  },
+  
+  {
+    id: 6,
     title: "CRUD - Customer Management System",
     tagline: "CMS with React and Node.js, build a customer DB with MySQL using AWS RDS and connect with Express & build REST API",
     liveUrl: "https://github.com/Amber-Kim/customer_management",
     repositoryUrl: "https://github.com/Amber-Kim/customer_management",
-    image: proj5,
+    image: proj6,
   },
   {
-    id: 6,
+    id: 7,
     title: "Google Geolocation APIs",
     tagline: "Created a mini Hamilton Map with functions that Marker, Info window, Filter, Geolocation, Geocoding, Directions, and Travel mode.",
     liveUrl: "https://blissful-davinci-3d502a.netlify.app/",
     repositoryUrl: "https://github.com/Amber-Kim/Google-Map-app-project",
-    image: proj6,
+    image: proj7,
   },
   
   {
-    id: 7,
+    id: 8,
     title: "Responsive Design and Bootstrap",
     tagline: "Build a Caledonia Animal Hospital website",
     liveUrl: "https://hungry-perlman-19d9ac.netlify.app/",
     repositoryUrl: "https://github.com/Amber-Kim/responsive-main-page-1",
-    image: proj7,
+    image: proj8,
   },
 ];
 export default PortfolioData;
