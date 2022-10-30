25500e0d679ad4ddf63b8de783b1ae3e15f2d58d
Amber-Kim <[amber.kje@gmail.com]>
2022-10-30 01:06:39 -0400

    Latest Updates

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
         animate="animate"
       >
         <h1>Hi!<br />I’m Jeong eun Kim</h1>
-        <p>I am a Junior Web Developer :) <br />Thank you for coming to my corner on the internet.</p>
+        <p>I am a Software Developer :) <br />Thank you for coming to my corner on the internet.</p>
         <Link
           className="home-btn"
           to={"portfolio"}
